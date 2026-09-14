0c4821b0b324cf6669f93636034358ee7f822811
nhatanh222k <nhatanh222006@gmail.com>
2026-09-14 17:06:14 +0700

    a

--- test/test.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LinearRegression
-X = [[20], [30], [40], [50], [60]]
-y = [500, 700, 900, 1100, 1300]
-model = LinearRegression()
-model.fit(X, y)
-dien_tich = float(input("Nhap dien tich nha: "))
-gia = model.predict([[dien_tich]])
-
-print("Gia nha du doan:", gia[0], "trieu")
